0a7724f7cc3691c61743ace50ca969285f6cd273
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2016-09-18 16:49:04 +0800

    发布需求页面结构搭建

--- app/src/main/java/com/slash/youth/ui/viewmodel/PublishDemandModel.java
+++ app/src/main/java/com/slash/youth/ui/viewmodel/PublishDemandModel.java
@@ -1,8 +1,12 @@
 package com.slash.youth.ui.viewmodel;
 
+import android.content.Intent;
 import android.databinding.BaseObservable;
+import android.view.View;
 
 import com.slash.youth.databinding.ActivityPublishDemandBinding;
+import com.slash.youth.ui.activity.MapActivity;
+import com.slash.youth.utils.CommonUtils;
 
 /**
  * Created by zhouyifeng on 2016/9/17.
@@ -12,5 +16,11 @@ public class PublishDemandModel extends BaseObservable {
 
     public PublishDemandModel(ActivityPublishDemandBinding activityPublishDemandBinding) {
         this.mActivityPublishDemandBinding = activityPublishDemandBinding;
+    }
+
+    public void getDetailLocation(View v) {
+        Intent intentMapActivity = new Intent(CommonUtils.getContext(), MapActivity.class);
+        intentMapActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        CommonUtils.getContext().startActivity(intentMapActivity);
     }
 }
