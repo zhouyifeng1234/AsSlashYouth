051b30d764ddbc9d7eac7f258ffa7386d04ffd9f
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2016-09-22 06:59:38 +0800

    添加图片功能测试

--- app/src/main/java/com/slash/youth/ui/viewmodel/PublishDemandDescModel.java
+++ app/src/main/java/com/slash/youth/ui/viewmodel/PublishDemandDescModel.java
@@ -41,4 +41,36 @@ public class PublishDemandDescModel extends BaseObservable {
         CommonUtils.getContext().startActivity(intentPublishDemandModeActivity);
     }
 
+    public void addPicture(View v) {
+//        Intent intentAddPicture = new Intent();
+//        intentAddPicture.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        intentAddPicture.setType("image/*");
+//        intentAddPicture.setAction(Intent.ACTION_GET_CONTENT);
+//        CommonUtils.getContext().startActivity(intentAddPicture);
+
+//        private File tempFile;
+//        this.tempFile = new File(FilePathUtility.GetReAudioFilePath(
+//                PersonalEditActivity.this, "head.jpg", true));
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+/* 开启Pictures画面Type设定为image */
+        intent.setType("image/*");
+/* 使用Intent.ACTION_GET_CONTENT这个Action */
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+/* 出现截取界面 */
+        intent.putExtra("crop", "true");
+/*保存到SD*/
+//        intent.putExtra("output", Uri.fromFile(tempFile));
+/*设置图片像素*/
+        intent.putExtra("outputX", 200);
+        intent.putExtra("outputY", 200);
+/*设置图片格式*/
+        intent.putExtra("outputFormat", "JPEG");
+/* 设置比例 1:1 */
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+/* 取得相片后返回本画面 */
+        CommonUtils.getContext().startActivity(intent);
+    }
+
 }
