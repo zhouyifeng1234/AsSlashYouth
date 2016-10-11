c84d0e56d1c51349a9f0beecd2db2fa2bb334673
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2016-10-12 06:28:32 +0800

    LayerListTestActivity

---
+++ app/src/main/java/com/slash/youth/ui/activity/test/LayerlistTestActivity.java
@@ -0,0 +1,15 @@
+package com.slash.youth.ui.activity.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhouyifeng on 2016/10/12.
+ */
+public class LayerlistTestActivity  extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
