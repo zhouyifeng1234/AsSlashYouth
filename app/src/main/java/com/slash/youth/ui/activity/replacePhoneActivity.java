314e174e4c982b7775bbf0a946c84790013599ea
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2016-11-08 10:52:22 +0800

    代码合并，文件名改正

--- app/src/main/java/com/slash/youth/ui/activity/replacePhoneActivity.java
+++ app/src/main/java/com/slash/youth/ui/activity/replacePhoneActivity.java
@@ -3,14 +3,12 @@ package com.slash.youth.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import com.slash.youth.R;
 import com.slash.youth.databinding.ActivityReplacePhoneBinding;
-import com.slash.youth.ui.viewmodel.ActivityUserInfoEditorModel;
 import com.slash.youth.ui.viewmodel.ReplacePhoneModel;
 import com.slash.youth.utils.CommonUtils;
 import com.slash.youth.utils.LogKit;
