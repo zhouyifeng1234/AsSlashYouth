37b355438f4893828572deaa2821de8cec7816c6
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2016-11-10 00:00:15 +0800

    服务详情页开发，接口调试

--- app/src/main/java/com/slash/youth/ui/viewmodel/ServiceDetailModel.java
+++ app/src/main/java/com/slash/youth/ui/viewmodel/ServiceDetailModel.java
@@ -2,7 +2,10 @@ package com.slash.youth.ui.viewmodel;
 
 import android.app.Activity;
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.view.View;
 
+import com.slash.youth.BR;
 import com.slash.youth.databinding.ActivityServiceDetailBinding;
 
 /**
@@ -26,5 +29,109 @@ public class ServiceDetailModel extends BaseObservable {
 
     private void initView() {
 
+    }
+
+    public void goBack(View v) {
+        mActivity.finish();
+    }
+
+    //分享服务（顶部需求者视角看到的分享按钮）
+    public void shareService(View v) {
+
+    }
+
+    //修改服务
+    public void updateService(View v) {
+
+    }
+
+    //下架服务
+    public void offShelfService(View v) {
+
+    }
+
+    //聊一聊
+    public void haveAChat(View v) {
+
+    }
+
+    //收藏服务
+    public void collectService(View v) {
+
+    }
+
+    //立即抢单（抢服务）
+    public void grabService(View v) {
+
+    }
+
+    //底部服务者视角看到的分享按钮
+    public void shareServiceBottom(View v) {
+
+    }
+
+    public void openServiceDetailLocation(View v) {
+
+    }
+
+    public void gotoUserInfo(View v) {
+
+    }
+
+
+    private int topShareBtnVisibility;
+    private int topServiceBtnVisibility;
+    private int bottomBtnDemandVisibility;//底部需求者视角可以看到的按钮
+    private int bottomBtnServiceVisibility;//底部服务者视角可以看到的按钮
+    private int offShelfLogoVisibility;
+
+    @Bindable
+    public int getTopShareBtnVisibility() {
+        return topShareBtnVisibility;
+    }
+
+    public void setTopShareBtnVisibility(int topShareBtnVisibility) {
+        this.topShareBtnVisibility = topShareBtnVisibility;
+        notifyPropertyChanged(BR.topShareBtnVisibility);
+    }
+
+    @Bindable
+    public int getTopServiceBtnVisibility() {
+        return topServiceBtnVisibility;
+    }
+
+    public void setTopServiceBtnVisibility(int topServiceBtnVisibility) {
+        this.topServiceBtnVisibility = topServiceBtnVisibility;
+        notifyPropertyChanged(BR.topServiceBtnVisibility);
+    }
+
+    @Bindable
+    public int getBottomBtnDemandVisibility() {
+        return bottomBtnDemandVisibility;
+    }
+
+    public void setBottomBtnDemandVisibility(int bottomBtnDemandVisibility) {
+        this.bottomBtnDemandVisibility = bottomBtnDemandVisibility;
+        notifyPropertyChanged(BR.bottomBtnDemandVisibility);
+    }
+
+    @Bindable
+    public int getBottomBtnServiceVisibility() {
+        return bottomBtnServiceVisibility;
+    }
+
+    public void setBottomBtnServiceVisibility(int bottomBtnServiceVisibility) {
+        this.bottomBtnServiceVisibility = bottomBtnServiceVisibility;
+        notifyPropertyChanged(BR.bottomBtnServiceVisibility);
+    }
+
+    @Bindable
+    public int getOffShelfLogoVisibility() {
+        return offShelfLogoVisibility;
+    }
+
+    public void setOffShelfLogoVisibility(int offShelfLogoVisibility) {
+        this.offShelfLogoVisibility = offShelfLogoVisibility;
+        notifyPropertyChanged(BR.offShelfLogoVisibility);
     }
 }
