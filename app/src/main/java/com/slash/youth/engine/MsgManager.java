6d6d4d05a346dc0aed53d80c95e659e3d8276cff
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2016-11-15 11:33:46 +0800

    消息推送

--- app/src/main/java/com/slash/youth/engine/MsgManager.java
+++ app/src/main/java/com/slash/youth/engine/MsgManager.java
@@ -4,6 +4,12 @@ import com.slash.youth.utils.LogKit;
 
 import io.rong.imlib.RongIMClient;
 import io.rong.imlib.model.Message;
+import io.rong.message.CommandMessage;
+import io.rong.message.CommandNotificationMessage;
+import io.rong.message.ImageMessage;
+import io.rong.message.LocationMessage;
+import io.rong.message.TextMessage;
+import io.rong.message.VoiceMessage;
 
 /**
  * Created by Administrator on 2016/8/31.
@@ -70,7 +76,54 @@ public class MsgManager {
         public boolean onReceived(Message message, int left) {
             //开发者根据自己需求自行处理
             String senderUserId = message.getSenderUserId();
-            LogKit.v(senderUserId);
+            LogKit.v("senderUserId:" + senderUserId);
+
+//            String extra = message.getExtra();
+//            LogKit.v("extra:" + extra);
+
+            String objectName = message.getObjectName();
+            LogKit.v("objectName:" + objectName);
+
+            if (objectName.equals("RC:TxtMsg")) {
+                TextMessage textMessage = (TextMessage) message.getContent();
+                String content = textMessage.getContent();
+                String extra = textMessage.getExtra();
+                LogKit.v("conent:" + content);
+                LogKit.v("extra:" + extra);
+            } else if (objectName.equals("RC:ImgMsg")) {
+                ImageMessage imageMessage = (ImageMessage) message.getContent();
+
+            } else if (objectName.equals("RC:VcMsg")) {
+                VoiceMessage voiceMessage = (VoiceMessage) message.getContent();
+
+            } else if (objectName.equals("RC:ImgTextMsg")) {
+
+
+            } else if (objectName.equals("RC:LBSMsg")) {
+                LocationMessage locationMessage = (LocationMessage) message.getContent();
+
+            } else if (objectName.equals("RC:InfoNtf")) {
+
+            } else if (objectName.equals("RC:ProfileNtf")) {
+
+            } else if (objectName.equals("RC:CmdNtf")) {
+                CommandNotificationMessage commandNotificationMessage = (CommandNotificationMessage) message.getContent();
+                String name = commandNotificationMessage.getName();
+                String data = commandNotificationMessage.getData();
+                LogKit.v("CmdNtf name:" + name + "  data:" + data);
+
+            } else if (objectName.equals("RC:CmdMsg")) {
+                CommandMessage commandMessage = (CommandMessage) message.getContent();
+                String name = commandMessage.getName();
+                String data = commandMessage.getData();
+                LogKit.v("CmdMsg name:" + name + " data:" + data);
+
+            } else if (objectName.equals("RC:ContactNtf")) {
+
+            } else {
+                //这里可能是自定义的消息类型
+            }
+
 
             return false;
         }
