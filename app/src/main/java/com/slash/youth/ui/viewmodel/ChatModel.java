f9d92da28c6cf823688b85048639e0000b992131
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2016-11-16 16:27:54 +0800

    聊天输入框的修改

--- app/src/main/java/com/slash/youth/ui/viewmodel/ChatModel.java
+++ app/src/main/java/com/slash/youth/ui/viewmodel/ChatModel.java
@@ -3,6 +3,8 @@ package com.slash.youth.ui.viewmodel;
 import android.app.Activity;
 import android.databinding.BaseObservable;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.slash.youth.databinding.ActivityChatBinding;
 
@@ -13,6 +15,8 @@ public class ChatModel extends BaseObservable {
 
     ActivityChatBinding mActivityChatBinding;
     Activity mActivity;
+    private TextView mTvChatFriendName;
+    private LinearLayout mLlChatContent;//聊天内容容器
 
     public ChatModel(ActivityChatBinding activityChatBinding, Activity activity) {
         this.mActivityChatBinding = activityChatBinding;
@@ -26,7 +30,13 @@ public class ChatModel extends BaseObservable {
     }
 
     private void initView() {
+        //使底部的输入框失去焦点，隐藏软键盘
+        mTvChatFriendName = mActivityChatBinding.tvChatFriendName;
+        mTvChatFriendName.setFocusable(true);
+        mTvChatFriendName.setFocusableInTouchMode(true);
+        mTvChatFriendName.requestFocus();
 
+        mLlChatContent = mActivityChatBinding.llChatContent;
     }
 
     public void goBack(View v) {
