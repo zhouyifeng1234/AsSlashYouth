95021547b1cdbf555ee11f6c2b9ad7f9a13e2334
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2016-12-11 23:07:40 +0800

    登录页面

--- app/src/main/java/com/slash/youth/ui/activity/SplashActivity.java
+++ app/src/main/java/com/slash/youth/ui/activity/SplashActivity.java
@@ -1,7 +1,14 @@
 package com.slash.youth.ui.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.slash.youth.R;
+import com.slash.youth.ui.view.fly.RandomLayout;
+import com.slash.youth.utils.CommonUtils;
 
 /**
  * Created by zhouyifeng on 2016/12/11.
@@ -11,6 +18,20 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ImageView ivSplash = new ImageView(CommonUtils.getContext());
+        ivSplash.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        ViewGroup.LayoutParams ll = new RandomLayout.LayoutParams(-1, -1);
+        ivSplash.setLayoutParams(ll);
+        ivSplash.setImageResource(R.mipmap.splash);
+        setContentView(ivSplash);
 
+        CommonUtils.getHandler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intentLoginActivity = new Intent(CommonUtils.getContext(), LoginActivity.class);
+                startActivity(intentLoginActivity);
+                finish();
+            }
+        }, 3000);
     }
 }
