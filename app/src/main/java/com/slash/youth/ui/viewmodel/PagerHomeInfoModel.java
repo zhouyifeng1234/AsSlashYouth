a0f31c77c75fb2d004c35acdba688cc1c0adc3cf
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2017-01-09 18:26:19 +0800

    消息列表

--- app/src/main/java/com/slash/youth/ui/viewmodel/PagerHomeInfoModel.java
+++ app/src/main/java/com/slash/youth/ui/viewmodel/PagerHomeInfoModel.java
@@ -17,6 +17,11 @@ import com.slash.youth.ui.adapter.HomeInfoListAdapter;
 import com.slash.youth.utils.CommonUtils;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import io.rong.imlib.RongIMClient;
+import io.rong.imlib.model.Conversation;
+import io.rong.imlib.model.Message;
 
 /**
  * Created by zhouyifeng on 2016/10/11.
@@ -55,13 +60,14 @@ public class PagerHomeInfoModel extends BaseObservable {
     }
 
     public void getDataFromServer() {
-        listConversation.clear();
         MsgManager.getConversationList(new BaseProtocol.IResultExecutor<ConversationListBean>() {
             @Override
             public void execute(ConversationListBean dataBean) {
+                listConversation.clear();
                 listConversation = dataBean.data.list;
                 if (listConversation != null) {
                     for (int i = 0; i < listConversation.size(); i++) {
+                        MsgManager.conversationUidList.clear();
                         ConversationListBean.ConversationInfo conversationInfo = listConversation.get(i);
                         MsgManager.conversationUidList.add(conversationInfo.uid + "");
                     }
@@ -81,18 +87,54 @@ public class PagerHomeInfoModel extends BaseObservable {
     public void setConversationList() {
         //这里当做listConversation中的数据是全部的会话列表数据，MsgManager.conversationUidList中就是全部会话的uid，并且是按时间顺序排列的
         if (!MsgManager.conversationUidList.contains("1000")) {
-            ConversationListBean conversationListBean = new ConversationListBean();
-            ConversationListBean.ConversationInfo conversationInfo = conversationListBean.new ConversationInfo();
-            conversationInfo.uid = 1000;
-            conversationInfo.uts = System.currentTimeMillis();
-            listConversation.add(conversationInfo);
-            MsgManager.conversationUidList.add("1000");
-        }
-        if (homeInfoListAdapter == null) {
-            homeInfoListAdapter = new HomeInfoListAdapter(listConversation);
-            mPagerHomeInfoBinding.lvPagerHomeInfo.setAdapter(homeInfoListAdapter);
-        } else {
-            homeInfoListAdapter.notifyDataSetChanged();
+            RongIMClient.getInstance().getLatestMessages(Conversation.ConversationType.PRIVATE, "1000", 1, new RongIMClient.ResultCallback<List<Message>>() {
+                @Override
+                public void onSuccess(List<Message> messages) {
+                    ConversationListBean conversationListBean = new ConversationListBean();
+                    ConversationListBean.ConversationInfo conversationInfo = conversationListBean.new ConversationInfo();
+                    conversationInfo.uid = 1000;
+                    if (messages != null && messages.size() > 0) {
+                        //本地有消息
+                        Message message = messages.get(0);
+                        long msgtime = 0;
+                        Message.MessageDirection messageDirection = message.getMessageDirection();
+                        if (messageDirection == Message.MessageDirection.SEND) {
+                            msgtime = message.getSentTime();
+                        } else if (messageDirection == Message.MessageDirection.RECEIVE) {
+                            msgtime = message.getReceivedTime();
+                        }
+                        conversationInfo.uts = msgtime;
+                        //这里认为listConversation中的数据都是按照时间顺序排列的
+                        int index = listConversation.size() - 1;
+                        for (int i = 0; i < listConversation.size(); i++) {
+                            ConversationListBean.ConversationInfo conversation = listConversation.get(i);
+                            if (conversation.uts < conversationInfo.uts) {
+                                index = i;
+                                break;
+                            }
+                        }
+                        listConversation.add(index, conversationInfo);
+                        MsgManager.conversationUidList.add(index, "1000");
+                    } else {
+                        //本地没有消息
+                        conversationInfo.uts = System.currentTimeMillis();
+                        listConversation.add(conversationInfo);
+                        MsgManager.conversationUidList.add("1000");
+                    }
+                    if (homeInfoListAdapter == null) {
+                        homeInfoListAdapter = new HomeInfoListAdapter(listConversation);
+                        mPagerHomeInfoBinding.lvPagerHomeInfo.setAdapter(homeInfoListAdapter);
+                    } else {
+                        homeInfoListAdapter.notifyDataSetChanged();
+                    }
+                }
+
+                @Override
+                public void onError(RongIMClient.ErrorCode errorCode) {
+
+                }
+            });
+
         }
     }
 
