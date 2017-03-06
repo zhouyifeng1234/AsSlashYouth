7a3ed562edd6db8f18d24e1ead1d94eff60c1ea9
zhouyifeng1234 <zhouyifeng@slashyounger.com>
2017-03-06 18:21:36 +0800

    修改会话列表数据问题、修改我的页面里面的好友相关列表、下拉刷新时出现空白再出现的问题

--- app/src/main/java/com/slash/youth/ui/viewmodel/ActivityContactsCareModel.java
+++ app/src/main/java/com/slash/youth/ui/viewmodel/ActivityContactsCareModel.java
@@ -2,18 +2,12 @@ package com.slash.youth.ui.viewmodel;
 
 import android.content.Intent;
 import android.databinding.BaseObservable;
-import android.net.sip.SipSession;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 
-import com.slash.youth.R;
 import com.slash.youth.databinding.ActivityContactsCareBinding;
 import com.slash.youth.domain.ContactsBean;
-import com.slash.youth.domain.RecommendFriendBean;
-import com.slash.youth.domain.SetBean;
 import com.slash.youth.engine.ContactsManager;
-import com.slash.youth.engine.MyManager;
 import com.slash.youth.global.GlobalConstants;
 import com.slash.youth.http.protocol.BaseProtocol;
 import com.slash.youth.ui.activity.ContactsCareActivity;
@@ -23,7 +17,6 @@ import com.slash.youth.ui.view.PullableListView.PullToRefreshLayout;
 import com.slash.youth.utils.CommonUtils;
 import com.slash.youth.utils.CustomEventAnalyticsUtils;
 import com.slash.youth.utils.LogKit;
-import com.slash.youth.utils.ToastUtils;
 import com.umeng.analytics.MobclickAgent;
 
 import java.util.ArrayList;
@@ -33,21 +26,25 @@ import java.util.List;
  * Created by acer on 2016/11/20.
  */
 public class ActivityContactsCareModel extends BaseObservable {
+    private static final int LOAD_DATA_TYPE_INIT = 1;//表示首次进入页面时的加载数据
+    private static final int LOAD_DATA_TYPE_REFRESH = 2;//表示下拉刷新的加载数据
+    private static final int LOAD_DATA_TYPE_MORE = 3;//表示上拉加载更多的加载数据
+
     private ActivityContactsCareBinding activityContactsCareBinding;
     private String title;
     private ArrayList<ContactsBean.DataBean.ListBean> contactsLists = new ArrayList<>();
     private ContactsCareAdapter contactsCareAdapter;
     private int offset = 0;
-    private int limit  = 20;
+    private int limit = 20;
     private int visibleLastIndex;
-    private int type =-1;
+    private int type = -1;
     private boolean isAgree;
     private int i = -1;
-    private  ContactsCareActivity contactsCareActivity;
+    private ContactsCareActivity contactsCareActivity;
     private int listSize;
     private int titleType;
 
-    public ActivityContactsCareModel(ActivityContactsCareBinding activityContactsCareBinding, String title, ContactsCareActivity contactsCareActivity,int type) {
+    public ActivityContactsCareModel(ActivityContactsCareBinding activityContactsCareBinding, String title, ContactsCareActivity contactsCareActivity, int type) {
         this.activityContactsCareBinding = activityContactsCareBinding;
         this.contactsCareActivity = contactsCareActivity;
         this.title = title;
@@ -57,6 +54,7 @@ public class ActivityContactsCareModel extends BaseObservable {
         initView();
         listener();
     }
+
     //加载listView
     private void initListView() {
         PullToRefreshLayout ptrl = activityContactsCareBinding.refreshView;
@@ -70,12 +68,7 @@ public class ActivityContactsCareModel extends BaseObservable {
                 @Override
                 public void run() {
                     offset = 0;
-                    contactsLists.clear();
-                    getData( title);
-                    if(contactsCareAdapter!=null){
-                        contactsCareAdapter.notifyDataSetChanged();
-                    }
-                    pullToRefreshLayout.refreshFinish(PullToRefreshLayout.SUCCEED);
+                    getData(title, pullToRefreshLayout, LOAD_DATA_TYPE_REFRESH);
                 }
             }, 2000);
         }
@@ -85,15 +78,11 @@ public class ActivityContactsCareModel extends BaseObservable {
             CommonUtils.getHandler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    if(listSize < limit){
+                    if (listSize < limit) {
                         pullToRefreshLayout.loadmoreFinish(PullToRefreshLayout.SUCCEED);
-                    }else {//不是最后一页
+                    } else {//不是最后一页
                         offset += limit;
-                        getData( title);
-                        if(contactsCareAdapter!=null){
-                            contactsCareAdapter.notifyDataSetChanged();
-                        }
-                        pullToRefreshLayout.loadmoreFinish(PullToRefreshLayout.SUCCEED);
+                        getData(title, pullToRefreshLayout, LOAD_DATA_TYPE_MORE);
                     }
                 }
             }, 2000);
@@ -101,26 +90,26 @@ public class ActivityContactsCareModel extends BaseObservable {
     }
 
     private void initData() {
-        getData( title);
+        getData(title, null, LOAD_DATA_TYPE_INIT);
     }
 
-    private void getData(String title) {
-        switch (title){
+    private void getData(String title, PullToRefreshLayout pullToRefreshLayout, int loadDataType) {
+        switch (title) {
             case ContactsManager.CARE_ME:
-                type=1;
-                ContactsManager.getAddMeList(new onGetAddMeList(),offset,limit,GlobalConstants.HttpUrl.MY_CARE_PERSON);
+                type = 1;
+                ContactsManager.getAddMeList(new onGetAddMeList(pullToRefreshLayout, loadDataType), offset, limit, GlobalConstants.HttpUrl.MY_CARE_PERSON);
                 break;
             case ContactsManager.MY_CARE:
-                type=2;
-                ContactsManager.getAddMeList(new onGetAddMeList(),offset,limit,GlobalConstants.HttpUrl.CARE_ME_PERSON);
+                type = 2;
+                ContactsManager.getAddMeList(new onGetAddMeList(pullToRefreshLayout, loadDataType), offset, limit, GlobalConstants.HttpUrl.CARE_ME_PERSON);
                 break;
             case ContactsManager.ADD_ME:
                 type = 3;
-                ContactsManager.getAddMeList(new onGetAddMeList(),offset,limit,GlobalConstants.HttpUrl.MY_FRIEND_LIST_ADD_ME_LIST);
+                ContactsManager.getAddMeList(new onGetAddMeList(pullToRefreshLayout, loadDataType), offset, limit, GlobalConstants.HttpUrl.MY_FRIEND_LIST_ADD_ME_LIST);
                 break;
             case ContactsManager.MY_ADD:
                 type = 4;
-                ContactsManager.getAddMeList(new onGetAddMeList(),offset,limit,GlobalConstants.HttpUrl.MY_FRIEND_LIST_MY_ADD_LIST);
+                ContactsManager.getAddMeList(new onGetAddMeList(pullToRefreshLayout, loadDataType), offset, limit, GlobalConstants.HttpUrl.MY_FRIEND_LIST_MY_ADD_LIST);
                 break;
         }
     }
@@ -135,7 +124,7 @@ public class ActivityContactsCareModel extends BaseObservable {
         activityContactsCareBinding.lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (type){
+                switch (type) {
                     case 1://关注我
                         MobclickAgent.onEvent(CommonUtils.getContext(), CustomEventAnalyticsUtils.EventID.RELATIONSHIP_CLICK_FOCUS_MINE_CLICK_PERSON_DETAIL);
                         break;
@@ -160,44 +149,72 @@ public class ActivityContactsCareModel extends BaseObservable {
     }
 
     public class onGetAddMeList implements BaseProtocol.IResultExecutor<ContactsBean> {
+
+        PullToRefreshLayout pullToRefreshLayout;
+        int loadDataType;
+
+        public onGetAddMeList(PullToRefreshLayout pullToRefreshLayout, int loadDataType) {
+            this.pullToRefreshLayout = pullToRefreshLayout;
+            this.loadDataType = loadDataType;
+        }
+
         @Override
         public void execute(ContactsBean dataBean) {
             int rescode = dataBean.getRescode();
-            if(rescode == 0){
+            if (rescode == 0) {
                 ContactsBean.DataBean data = dataBean.getData();
                 List<ContactsBean.DataBean.ListBean> list = data.getList();
-                listSize = list.size();
-                activityContactsCareBinding.tvTitle.setVisibility(View.VISIBLE);
-                switch (type){
-                    case 1:
-                        activityContactsCareBinding.tvTitle.setText("暂无关注");
-                        activityContactsCareBinding.tvContent.setText("你还没有关注过任何人哦，赶紧去关注吧");
-                        break;
-                    case 2:
-                        activityContactsCareBinding.tvTitle.setText("暂无粉丝");
-                        activityContactsCareBinding.tvContent.setText("竟然一个粉丝都没有，快出去喊喊口号");
-                        break;
-                    case 3:
-                        activityContactsCareBinding.tvTitle.setText("暂无好友申请");
-                        activityContactsCareBinding.tvContent.setText("快去寻觅自己的好友吧");
-                        break;
-                    case 4:
-                        activityContactsCareBinding.tvTitle.setText("还未发出过好友申请");
-                        activityContactsCareBinding.tvContent.setText("呜呜，这么多优秀的人你都看不到");
-                        break;
-                }
-                activityContactsCareBinding.rlHomeDefaultImage.setVisibility(listSize == 0?View.VISIBLE:View.GONE);
-
-                if(listSize!=0){
+                if (list == null || list.size() <= 0) {
+                    if (offset == 0) {
+                        activityContactsCareBinding.rlHomeDefaultImage.setVisibility(View.VISIBLE);
+                        switch (type) {
+                            case 1:
+                                activityContactsCareBinding.tvTitle.setText("暂无关注");
+                                activityContactsCareBinding.tvContent.setText("你还没有关注过任何人哦，赶紧去关注吧");
+                                break;
+                            case 2:
+                                activityContactsCareBinding.tvTitle.setText("暂无粉丝");
+                                activityContactsCareBinding.tvContent.setText("竟然一个粉丝都没有，快出去喊喊口号");
+                                break;
+                            case 3:
+                                activityContactsCareBinding.tvTitle.setText("暂无好友申请");
+                                activityContactsCareBinding.tvContent.setText("快去寻觅自己的好友吧");
+                                break;
+                            case 4:
+                                activityContactsCareBinding.tvTitle.setText("还未发出过好友申请");
+                                activityContactsCareBinding.tvContent.setText("呜呜，这么多优秀的人你都看不到");
+                                break;
+                        }
+                    }
+                } else {
+                    listSize = list.size();
+                    activityContactsCareBinding.rlHomeDefaultImage.setVisibility(View.GONE);
+                    if (loadDataType == LOAD_DATA_TYPE_REFRESH || loadDataType == LOAD_DATA_TYPE_INIT) {
+                        contactsLists.clear();
+                    }
                     contactsLists.addAll(list);
-                    contactsCareAdapter = new ContactsCareAdapter(contactsLists,type);
-                    activityContactsCareBinding.lv.setAdapter(contactsCareAdapter);
+                    if (contactsCareAdapter != null) {
+                        contactsCareAdapter.notifyDataSetChanged();
+                    } else {
+                        contactsCareAdapter = new ContactsCareAdapter(contactsLists, type);
+                        activityContactsCareBinding.lv.setAdapter(contactsCareAdapter);
+                    }
+                    if (loadDataType == LOAD_DATA_TYPE_REFRESH) {
+                        if (pullToRefreshLayout != null) {
+                            pullToRefreshLayout.refreshFinish(PullToRefreshLayout.SUCCEED);
+                        }
+                    } else if (loadDataType == LOAD_DATA_TYPE_MORE) {
+                        if (pullToRefreshLayout != null) {
+                            pullToRefreshLayout.loadmoreFinish(PullToRefreshLayout.SUCCEED);
+                        }
+                    }
                 }
             }
         }
+
         @Override
         public void executeResultError(String result) {
-            LogKit.d("result:"+result);
+            LogKit.d("result:" + result);
         }
     }
 }
